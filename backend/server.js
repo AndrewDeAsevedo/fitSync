43553e6d0e18fbc1fff0aa4019863ad02709ffe6
SuyogyaAcharya <acharya.suyogya@gmail.com>
2025-08-28 15:06:17 -0400

    adding server.js

--- backend/server.js
+++ backend/server.js
@@ -10,3 +10,6 @@ app.get('/', (req, res) => res.send('FitSync Backend Running'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+const userRoutes = require('./routes/userRoutes');
+app.use('/api/users', userRoutes);
